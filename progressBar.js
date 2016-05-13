0afb40f687f958b18afe0896505de862adc05545
Norman Xin <norman.xin@turn.com>
2016-05-12 20:19:51 -0700

    fix-progress

--- progressBar.js
+++ progressBar.js
@@ -5,20 +5,17 @@ angular.module('iom-app')
     //transclude: true,
     scope: {},
     templateUrl: 'progressButton.html',
-    link: function(scope) {
-    	    angular.element('head style[type="text/css"]').attr('type', 'text/less');
-		less.refreshStyles();
+    link: function(scope, element) {
+    	  $('head style[type="text/css"]').attr('type', 'text/less');
+		//less.refreshStyles();
 		window.randomize = function() {
-			angular.element('.radial-progress').attr('data-progress', Math.floor(Math.random() * 100));
+		    $('.radial-progress').attr('data-progress', Math.floor(Math.random() * 100));
 		};
 		setTimeout(window.randomize, 200);
 		console.log('wow');
-		angular.element('.radial-progress').click(window.randomize);
+		$('.radial-progress').click(window.randomize);
 
 		// Read more here: https://medium.com/@andsens/radial-progress-indicator-using-css-a917b80c43f9
     }
   };
 });;
-
-
-
