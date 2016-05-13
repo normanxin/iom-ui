9aa62af81abd86ce6d0a71903c1ee176187eaf11
Norman Xin <norman.xin@turn.com>
2016-05-13 10:26:53 -0700

    bind data

--- context.js
+++ context.js
@@ -7,24 +7,10 @@ angular.module('iom-app')
     templateUrl: 'context.html',
     controller: function($rootScope, $scope) {
       $scope.$watch(function() {
-        return $rootScope.selectedLI
+        return $rootScope.selectedLI;
       }, function(selected) {
         $scope.names = selected;
-        $scope.names = [
-          {timestap: Math.random() * 3, state: 2, clicks: 3, impressions: 4},
-          {timestap: 3, state: 2, clicks: 3, impressions: 4},
-          {timestap: 3, state: 2, clicks: 3, impressions: 4},
-          {timestap: 3, state: 2, clicks: 3, impressions: 4},
-        ];
       });
-    },
-    link: function(scope) {
-    	    scope.names = [
-    	       {timestap: 1, state: 2, clicks: 3, impressions: 4},
-    	       {timestap: 1, state: 2, clicks: 3, impressions: 4},
-    	       {timestap: 1, state: 2, clicks: 3, impressions: 4},
-    	       {timestap: 1, state: 2, clicks: 3, impressions: 4},
-    	    ];
     }
   };
 });;
--- input.js
+++ input.js
@@ -5,18 +5,23 @@ angular.module('iom-app')
     //transclude: true,
     scope: {},
     templateUrl: 'input.html',
-    controller: function($rootScope, $scope) {
+    controller: function($rootScope, $scope, $http) {
+        var li1, li2;
         $scope.select = function(s) {
           s.selected = true;
-          $rootScope.selectedLI = s;
+          $rootScope.selectedLI = s.id === 1 ? li1 : li2;
         };
+        $http.get('/jax/lineitem/1').success(function(res) {
+          li1 = res;
+        });
+        $http.get('/jax/lineitem/2').success(function(res) {
+          li2 = res;
+        });
     },
     link: function(scope) {
     	    scope.names = [
-    	       {id: 1, endDate: 2, clicks: 3, impressions: 4},
-    	       {id: 1, endDate: 2, clicks: 3, impressions: 4},
-    	       {id: 1, endDate: 2, clicks: 3, impressions: 4},
-    	       {id: 1, endDate: 2, clicks: 3, impressions: 4},
+    	       {id: 1, endDate: '2016-05-04', clicks: 51, impressions: 1079},
+    	       {id: 2, endDate: '2016-05-11', clicks: 103, impressions: 1225}
     	    ];
     }
   };
