6d334c13bdcdc6b679548e2b77bfa97815565aef
Norman Xin <norman.xin@turn.com>
2016-05-13 11:21:52 -0700

    roi chart

--- roi.js
+++ roi.js
@@ -5,24 +5,27 @@ angular.module('iom-app')
     scope: {},
     templateUrl: 'roi.html',
     controller: function($rootScope, $scope, $http) {
-      $scope.labels = ["2015-11-12", "2016-1-12", "2016-1-14", "2016-2-14", "2016-4-12", "2016-4-18", "2016-6-18"];
-      $scope.series = ['Line Item'];
-      $scope.data = [
-        [65, 59, 80, 81, 56, 55, 40]
-      ];
-
-      $http.get('/jax/lineitem/1').success(function(res) {
-        console.log(res);
-      });
+      // $scope.series = ['Clicks', 'Impressions', 'ROI'];
+      $scope.series = ['ROI'];
       $scope.$watch(function() {
         return $rootScope.selectedLI;
       }, function(selected) {
-        $scope.names = selected;
-        $scope.names = [
-          {timestap: Math.random() * 3, state: 2, clicks: 3, impressions: 4},
-          {timestap: 3, state: 2, clicks: 3, impressions: 4},
-          {timestap: 3, state: 2, clicks: 3, impressions: 4},
-          {timestap: 3, state: 2, clicks: 3, impressions: 4},
+        if (!selected) {
+          return $scope.data = [];
+        }
+        $scope.labels = $rootScope.selectedLI.map(function(li) {
+          return li.timeStamp;
+        });
+        $scope.data = [
+          // $rootScope.selectedLI.map(function(li) {
+          //   return li.numberOfClicks;
+          // }),
+          // $rootScope.selectedLI.map(function(li) {
+          //   return li.numberOfImpressions;
+          // }),
+          $rootScope.selectedLI.map(function(li) {
+            return li.numberOfClicks / li.numberOfImpressions;
+          })
         ];
       });
     }
